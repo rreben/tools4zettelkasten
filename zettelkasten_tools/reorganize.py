fb1df04a8c0109f9e5fbf80baae285a04a6eaf30
rreben <rupert.rebentisch@gmail.com>
2021-07-08 23:14:27 +0200

    commenting

--- zettelkasten_tools/reorganize.py
+++ zettelkasten_tools/reorganize.py
@@ -2,6 +2,15 @@ from . import handle_filenames as hf
 
 
 def attach_missing_ids(file_name_list):
+    """Generates a list of commands to attach missing ids by renaming files
+
+    :param file_name_list: List of files in a given directory which might
+        contain filenames with missing ids.
+    :type file_name_list: list of strings
+    :return: list of commands. Each command is a list with three components.
+        the command (i.e. rename), the old filename and the new filename.
+    :rtype: list of list of strings
+    """
     command_list = []
     for filename in file_name_list:
         components = hf.get_filename_components(filename)
