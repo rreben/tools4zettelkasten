6781475af83f45cfd288b93fc42bdf6231667a37
rreben <rupert.rebentisch@gmail.com>
2021-07-11 23:24:29 +0200

    generate_tree refactoring and comments

--- tests/test_reorganize.py
+++ tests/test_reorganize.py
@@ -2,17 +2,73 @@
 # Copyright (c) 2021 Dr. Rupert Rebentisch
 # Licensed under the MIT license
 
+from zettelkasten_tools.reorganize import corrections_elements
 from .context import zettelkasten_tools as zt
 import re
 
 
+def test_attach_missing_ids():
+    test_list = [
+        '5_10_Senescent_cells_9e051e2c4.md',
+        '1_2_reframe_your_goal_as_a_learning_goal.md',
+        '2_1a_render_md_files_with_python_and_flask_41e5a496c.md',
+        '2_5_homebrew.md']
+    command_list = zt.reorganize.attach_missing_ids(test_list)
+    assert len(command_list) == 2
+    assert command_list[0][0] == 'rename'
+    assert zt.handle_filenames.get_filename_components(
+        command_list[0][2])[0] == '1_2'
+    assert zt.handle_filenames.get_filename_components(
+        command_list[0][2])[1] == 'reframe_your_goal_as_a_learning_goal'
+    assert re.match(r'.*_[0-9a-f]{9}\.md$', command_list[0][2])
+
+
 def test_generate_tokenized_list():
     test_list = [
-        '5_10_Senescent_cells.md',
-        '1_2_reframe_your_goal_as_a_learning_goal.md',
-        '2_1a_render_md_files_with_python_and_flask.md',
-        '2_5_homebrew.md']
+        '5_10_Senescent_cells_9e051e2c4.md',
+        '1_2_reframe_your_goal_as_a_learning_goal_ab9df245b.md',
+        '2_1a_render_md_files_with_python_and_flask_ab9df245b.md',
+        '2_5_homebrew_282f521b1.md']
     tokenized_list = zt.reorganize.generate_tokenized_list(test_list)
     assert tokenized_list[0][0][0] == '5'
     assert tokenized_list[1][0][1] == '2'
-    assert tokenized_list[0][1] == '5_10_Senescent_cells.md'
+    assert tokenized_list[0][1] == '5_10_Senescent_cells_9e051e2c4.md'
+
+
+def test_corrections_elements():
+    list_of_keys = ['1', '2', '4', '5', '5a', '6', '7', '8', '8a', '8b', '9']
+    corrections_elements = zt.reorganize.corrections_elements(list_of_keys)
+    assert corrections_elements['1'] == '01'
+    assert corrections_elements['8b'] == '10'
+
+
+def test_corrections_elements_shrink():
+    # subtrees have been removed
+    list_of_keys = ['01', '02', '10', '11']
+    corrections_elements = zt.reorganize.corrections_elements(list_of_keys)
+    assert corrections_elements['01'] == '1'
+    assert corrections_elements['11'] == '4'
+
+
+def test_generate_tree():
+    test_list = [
+        '1_first_topic_41b4e4f8f.md',
+        '1_1_a_Thought_on_first_topic_2c3c34ff5.md',
+        '1_2_another_Thought_on_first_topic_2af216153.md',
+        '2_Second_Topic_cc6290ab7.md',
+        '2_1_a_Thought_on_Second_Topic_176fb43ae.md']
+    tokenized_list = zt.reorganize.generate_tokenized_list(test_list)
+    assert tokenized_list == [
+        [['1'], '1_first_topic_41b4e4f8f.md'],
+        [['1', '1'], '1_1_a_Thought_on_first_topic_2c3c34ff5.md'],
+        [['1', '2'], '1_2_another_Thought_on_first_topic_2af216153.md'],
+        [['2'], '2_Second_Topic_cc6290ab7.md'],
+        [['2', '1'], '2_1_a_Thought_on_Second_Topic_176fb43ae.md']]
+    tree = zt.reorganize.generate_tree(tokenized_list)
+    assert tree == [
+        ['1', '1_first_topic_41b4e4f8f.md',
+            [
+                ['1', '1_1_a_Thought_on_first_topic_2c3c34ff5.md'],
+                ['2', '1_2_another_Thought_on_first_topic_2af216153.md']]],
+        ['2', '2_Second_Topic_cc6290ab7.md',
+            [['1', '2_1_a_Thought_on_Second_Topic_176fb43ae.md']]]]
--- zettelkasten_tools/reorganize.py
+++ zettelkasten_tools/reorganize.py
@@ -9,12 +9,31 @@ import re
 def attach_missing_ids(file_name_list):
     """Generates a list of commands to attach missing ids by renaming files
 
+    Suppose we have the following list as input:
+
+    ['5_10_Senescent_cells_9e051e2c4.md',
+    '1_2_reframe_your_goal_as_a_learning_goal.md',
+    '2_1a_render_md_files_with_python_and_flask_41e5a496c.md',
+    '2_5_homebrew.md']
+
+    Then the following command_list will be retrurned:
+
+    [['rename', '1_2_reframe_your_goal_as_a_learning_goal.md',
+    '1_2_reframe_your_goal_as_a_learning_goal_7ae870951.md'],
+    ['rename', '2_5_homebrew.md', '2_5_homebrew_fe38ebbaa.md']]
+
+    Remark: The Ids are seeded with the timestamp. So the Ids will
+    change with every new run of the program or test.
+
     :param file_name_list: List of files in a given directory which might
         contain filenames with missing ids.
     :type file_name_list: list of strings
     :return: list of commands. Each command is a list with three components.
         the command (i.e. rename), the old filename and the new filename.
     :rtype: list of list of strings
+
+    ToDo:
+    *   Implement mechanism to resolve hash collisions
     """
     command_list = []
     for filename in file_name_list:
@@ -29,6 +48,27 @@ def attach_missing_ids(file_name_list):
 
 
 def generate_tokenized_list(zettelkasten_list):
+    """generates a list of tokens from a file list
+
+    Suppose the list of filenames for input is:
+
+    ['5_10_Senescent_cells_9e051e2c4.md',
+    '1_2_reframe_your_goal_as_a_learning_goal_ab9df245b.md',
+    '2_1a_render_md_files_with_python_and_flask.md_41e5a496c',
+    '2_5_homebrew.md_282f521b1']
+
+    Then the function will return:
+
+    [[['5', '10'], '5_10_Senescent_cells_9e051e2c4.md'],
+    [['1', '2'], '1_2_reframe_your_goal_as_a_learning_goal_ab9df245b.md'],
+    [['2', '1a'], '2_1a_render_md_files_with_python_and_flask.md_41e5a496c'],
+    [['2', '5'], '2_5_homebrew.md_282f521b1']]
+
+    :param zettelkasten_list: the list of filennames
+    :type zettelkasten_list: list
+    :return: a list of list of ordering items and filenames
+    :rtype: list
+    """
     tokenized_list = []
     for filename in zettelkasten_list:
         filename_components = hf.get_filename_components(filename)
@@ -38,3 +78,122 @@ def generate_tokenized_list(zettelkasten_list):
         #    trunk_filen_name = match.group()
         tokenized_list.append(numbering_in_filename_and_filename)
     return tokenized_list
+
+
+def corrections_elements(list_of_keys):
+    """corrects numberings to canonical form
+
+    Suppose you have named your files in one level
+    and one subtree like as follows.
+
+    ['1', '2', '4', '5', '5a', '6', '7', '8', '8a', '8b', '9']
+
+    Then you get a dictionary which you can use
+    to bring the numbers to a canonical form:
+
+    {'1': '01',
+    '2': '02',
+    '4': '03',
+    '5': '04',
+    '5a': '05',
+    '6': '06',
+    '7': '07',
+    '8': '08',
+    '8a': '09',
+    '8b': '10',
+    '9': '11'}
+
+    :param list_of_keys: numbering keys of one subtree and one level
+    :type list_of_keys: list
+    :return: what key to set for which original key to get
+             a canonical numbering
+    :rtype: dictionary
+    """
+    list_of_keys_with_leading_zeros = []
+    corrections_elements_dict = {}
+    number_of_necessary_digits = len(str(abs(len(list_of_keys))))
+    # sort keys in a way that
+    # 8, 8a, 10, 11
+    # is sorted like
+    # 08, 08a, 10, 11
+    # and not like
+    # 10, 11, 8, 8a what a simple sort would do
+    for i in range(len(list_of_keys)):
+        number_of_digits = sum(c.isdigit() for c in list_of_keys[i])
+        leading_zeros = '0' * (number_of_necessary_digits - number_of_digits)
+        list_of_keys_with_leading_zeros.append(
+            [list_of_keys[i], leading_zeros + list_of_keys[i]])
+    list_of_keys_with_leading_zeros.sort(key=lambda x: x[1])
+    # now form canonical numbering scheme and make the corrections
+    # as elements of the dictionary
+    for i in range(len(list_of_keys_with_leading_zeros)):
+        j = i + 1
+        number_of_digits = sum(c.isdigit() for c in str(j))
+        leading_zeros = '0' * (number_of_necessary_digits - number_of_digits)
+        list_of_keys_with_leading_zeros[i].append(leading_zeros + str(j))
+        corrections_elements_dict[
+            list_of_keys_with_leading_zeros[i][0]] = (
+                list_of_keys_with_leading_zeros[i][2])
+    return corrections_elements_dict
+
+
+def generate_tree(tokenized_list):
+    """generates a tree from a tokenized list
+
+    Suppose we have the following files:
+
+    ['1_first_topic_41b4e4f8f.md',
+    '1_1_a_Thought_on_first_topic_2c3c34ff5.md',
+    '1_2_another_Thought_on_first_topic_2af216153.md',
+    '2_Second_Topic_cc6290ab7.md',
+    '2_1_a_Thought_on_Second_Topic_176fb43ae.md']
+
+    After tokenizing (which is done elsewhere)
+    we get
+
+    [[['1'], '1_first_topic_41b4e4f8f.md'],
+    [['1', '1'], '1_1_a_Thought_on_first_topic_2c3c34ff5.md'],
+    [['1', '2'], '1_2_another_Thought_on_first_topic_2af216153.md'],
+    [['2'], '2_Second_Topic_cc6290ab7.md'],
+    [['2', '1'], '2_1_a_Thought_on_Second_Topic_176fb43ae.md']]
+
+    This is our input. generate_tree will do a recursion
+    to form a tree like:
+
+    [['1', '1_first_topic_41b4e4f8f.md',
+        [['1', '1_1_a_Thought_on_first_topic_2c3c34ff5.md'],
+        ['2', '1_2_another_Thought_on_first_topic_2af216153.md']]],
+    ['2', '2_Second_Topic_cc6290ab7.md',
+        [['1', '2_1_a_Thought_on_Second_Topic_176fb43ae.md']]]]
+
+    :param tokenized_list: the tokenized list of hierchical files
+    :type tokenized_list: list
+    :return: structured tree
+    :rtype: list
+    """
+    # prepare canonical correction of keys in each
+    # level of recursion
+    tree_keys = [x[0][0] for x in tokenized_list]
+    tree_keys.sort()
+    # remove duplicates
+    tree_keys = list(dict.fromkeys(tree_keys))
+    corrections_elements_dict = corrections_elements(tree_keys)
+    tree = []
+    for tree_key in tree_keys:
+        sub_tree = []
+        sub_tokenized_list = []
+        sub_tree.append(corrections_elements_dict[tree_key])
+        for x in tokenized_list:
+            if x[0][0] == tree_key:
+                if len(x[0]) == 1:
+                    sub_tree.append(x[1])
+                else:
+                    sub_tokenized_list.append([x[0][1:], x[1]])
+                    # sub_tree.append(generate_tree([x[0][1:],x[1]]))
+        # sub_tree.append([[x[0][1:],
+        # x[1]] for x in tokenized_list if x[0][0] == tree_key])
+        if len(sub_tokenized_list) > 0:
+            sub_tree.append(generate_tree(sub_tokenized_list))
+        tree.append(sub_tree)
+    tree.sort(key=lambda x: x[0])
+    return tree
