4b9eff09bb20099d185bbaa562525be7fbd4ca28
rreben <rupert.rebentisch@gmail.com>
2021-09-02 19:13:06 +0200

    update

--- experiments/KWDate.py
+++ experiments/KWDate.py
@@ -4,7 +4,7 @@ wochentage = [
     "Montag", "Dienstag", "Mittwoch",
     "Donnerstag", "Freitag", "Samstag", "Sonntag"
 ]
-d = date(2021, 8, 16)
+d = date(2021, 8, 23)
 print("KW", d.isocalendar()[1])
 for i in range(0, 7):
     print(d.strftime("%Y%m%d"), wochentage[d.weekday()])
