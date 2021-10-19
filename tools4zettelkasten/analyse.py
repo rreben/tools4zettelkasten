1d1a3f41fbcdcb454dbe5da37d6d8d23a24b2c7d
rreben <rupert.rebentisch@gmail.com>
2021-10-19 11:26:50 +0200

    type hints

--- tools4zettelkasten/analyse.py
+++ tools4zettelkasten/analyse.py
@@ -4,12 +4,15 @@
 
 from graphviz import Digraph
 from . import handle_filenames as hf
+from . import reorganize as ro
 
 
-def show_graph_of_zettelkasten(list_of_filenames, list_of_links):
+def show_graph_of_zettelkasten(
+        list_of_filenames: list[str],
+        list_of_links: list[ro.Link]):
     dot = Digraph(comment='Zettelkasten')
 
-    # Goal
+    # Nodes
     for filename in list_of_filenames:
         filename_components = hf.get_filename_components(filename=filename)
         print(filename_components[2], filename_components[1])
@@ -19,15 +22,11 @@ def show_graph_of_zettelkasten(list_of_filenames, list_of_links):
             shape='box',
             style='rounded')
 
+    # Edges
     for link in list_of_links:
         source_components = hf.get_filename_components(link.source)
         target_components = hf.get_filename_components(link.target)
         dot.edge(source_components[2], target_components[2])
-    # Edges
-    # dot.edge('0', '1')
-    # dot.edge('0', '2')
-    # dot.edge('1', '1.1')
-    # dot.edge('2', '2.1')
 
     # Generate Output
     print(dot.source)
