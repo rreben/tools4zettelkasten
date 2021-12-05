901689c3e3f7e2e56b69fcade021731424f719c9
rreben <rupert.rebentisch@gmail.com>
2021-12-05 22:05:04 +0100

    add ordering to graph

--- tools4zettelkasten/analyse.py
+++ tools4zettelkasten/analyse.py
@@ -6,6 +6,7 @@ import pprint as pp
 from graphviz import Digraph
 from . import handle_filenames as hf
 from . import reorganize as ro
+from . import settings as st
 from textwrap import fill
 
 
@@ -21,9 +22,12 @@ def show_graph_of_zettelkasten(
     # Nodes
     for filename in list_of_filenames:
         note = hf.create_Note(filename)
+        title_of_node = (
+            note.ordering.replace('_', ' ') +
+            ' ' + note.base_filename.replace("_", " "))
         dot.node(
             note.id,
-            fill(note.base_filename.replace("_", " "), width=30),
+            fill(title_of_node, width=30),
             shape='box',
             style='rounded')
 
@@ -31,7 +35,12 @@ def show_graph_of_zettelkasten(
     for link in list_of_links:
         source_note = hf.create_Note(link.source)
         target_note = hf.create_Note(link.target)
-        dot.edge(source_note.id, target_note.id)
+        if link.description == st.DIRECT_DAUGHTER_ZETTEL:
+            dot.edge(source_note.id, target_note.id, color='blue')
+        elif link.description == st.DIRECT_SISTER_ZETTEL:
+            dot.edge(source_note.id, target_note.id)
+        else:
+            dot.edge(source_note.id, target_note.id, color='red')
 
     # Generate Output
     print(dot.source)
