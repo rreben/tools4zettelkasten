f61fa46fc860aa171597a7c1da5d3afd0ff37cad
rreben <rupert.rebentisch@gmail.com>
2022-04-07 21:13:18 +0200

    Fix of version display on testpypi

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/x-rst',
     author='Dr. Rupert Rebentisch',
+    author_email='rupert.rebentisch@gmail.com',
     url='https://github.com/rreben/tools4zettelkasten',
     py_modules=['tools4zettelkasten'],
     install_requires=[
